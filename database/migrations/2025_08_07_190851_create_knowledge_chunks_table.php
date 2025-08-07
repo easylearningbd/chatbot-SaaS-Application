c8dcdef46f0ab2916c6ddc8560f74fdb96be66f2
easylearningbd <elebdsoft@gmail.com>
2025-08-08 01:15:49 +0600

    35 Create Knowledge and Chat Table With Relationship Part 2

---
+++ database/migrations/2025_08_07_190851_create_knowledge_chunks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('knowledge_chunks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('knowledge_document_id')->constrained()->onDelete('cascade');
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->text('content');
+            $table->json('embedding');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('knowledge_chunks');
+    }
+};
